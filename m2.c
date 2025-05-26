182d9acf50bd616a471679cdfaaaa96c137f86cd
Vignesh S <vignesh.s2@s.amity.edu>
2025-05-26 22:27:34 +0530

    update

--- m2.c
+++ m2.c
@@ -2,7 +2,7 @@
 
 int main() {
     int num;
-    printf("Enter the  integer: ");
+    printf("Enter all integer: ");
     scanf("%d", &num);  // Get integer input from the user
     printf("You entered: %d\n", num);
     return 0;
